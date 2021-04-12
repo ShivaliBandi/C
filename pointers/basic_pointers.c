206a53269eed5630b2ff4f8bc685aa5aa4201a73
ShivaliBandi <shivbandi3004@gmail.com>
2021-04-12 11:56:04 +0530

    new files commited

---
+++ pointers/basic_pointers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+Pointer:It is considered as a variable which stores the address.
+As pointer holds an address,address cannot be negative so pointer requires unsigned long integer so store an address
+Depends on datatype to which we apply the pointer there are different types of pointers as integer pointer,
+character pointer,float pointer,double pointer etc
+Depend on the datatype to which out pointer is pointing we can decide how menory bytes of memoru that we can fetch
+*/
+int main()
+{
+
+    return 0;
+}
